dd7a06ce56f1386bbc445884f37ade555cda9187
Edward Chong <82353320+ewkchong@users.noreply.github.com>
2023-07-03 21:49:24 -0700

    log library, partial security implementation

---
+++ internal/auth/authorizer.go
@@ -0,0 +1,36 @@
+package auth
+
+import (
+	"fmt"
+
+	"github.com/casbin/casbin"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func New(model, policy string) *Authorizer {
+	enforcer := casbin.NewEnforcer(model, policy)
+	return &Authorizer{
+		enforcer: enforcer,
+	}
+}
+
+type Authorizer struct {
+	enforcer *casbin.Enforcer
+}
+
+//  NOTE: Our Authorizer class is meant to act as a wrapper for Casbin's Enforce
+func (a *Authorizer) Authorize(subject, object, action string) error {
+	if !a.enforcer.Enforce(subject, object, action) {
+		msg := fmt.Sprintf(
+			"%s not permitted to %s to %s",
+			subject,
+			action,
+			object,
+		)
+		st := status.New(codes.PermissionDenied, msg)
+		return st.Err()
+	}
+
+	return nil
+}
